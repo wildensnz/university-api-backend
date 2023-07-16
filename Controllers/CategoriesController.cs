0deaa7baca8b4d969d2675058ac0bc8f0aed2462
HP <HP@DESKTOP-LKFD5H8>
2023-07-16 17:49:19 -0400

    first commit

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using university_api_backend.DataAccess;
+using university_api_backend.Models.DataModels;
+
+namespace university_api_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly UniversityDbContext _context;
+
+        public CategoriesController(UniversityDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categories>>> GetCategories()
+        {
+          if (_context.Categories == null)
+          {
+              return NotFound();
+          }
+            return await _context.Categories.ToListAsync();
+        }
+
+        // GET: api/Categories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categories>> GetCategories(int id)
+        {
+          if (_context.Categories == null)
+          {
+              return NotFound();
+          }
+            var categories = await _context.Categories.FindAsync(id);
+
+            if (categories == null)
+            {
+                return NotFound();
+            }
+
+            return categories;
+        }
+
+        // PUT: api/Categories/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategories(int id, Categories categories)
+        {
+            if (id != categories.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categories).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categories
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Categories>> PostCategories(Categories categories)
+        {
+          if (_context.Categories == null)
+          {
+              return Problem("Entity set 'UniversityDbContext.Categories'  is null.");
+          }
+            _context.Categories.Add(categories);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategories", new { id = categories.Id }, categories);
+        }
+
+        // DELETE: api/Categories/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategories(int id)
+        {
+            if (_context.Categories == null)
+            {
+                return NotFound();
+            }
+            var categories = await _context.Categories.FindAsync(id);
+            if (categories == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(categories);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CategoriesExists(int id)
+        {
+            return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Models/DataModels/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace university_api_backend.Models.DataModels
+
+{
+    public class Categories: BaseEntity
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+    }
+}
